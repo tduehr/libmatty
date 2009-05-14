fdc3a064c0a06542f64940201c4bd8612a74274b
tduehr <td@matasano.com>
2009-05-13 22:10:35 -0500

    fixed error in Object

--- lib/libmatty/object.rb
+++ lib/libmatty/object.rb
@@ -1,23 +1,20 @@
+# These should probably be extensions to Module since that's the location of instance_eval and friends.
 class Object
     module ObjectExtensions
-      # don't need these if we're using 1.9
-      if not Object::RUBY_FRAMEWORK_VERSION == "1.9"
-        # Every object has a "singleton" class, which you can think
-        # of as the class (ie, 1.metaclass =~ Fixnum) --- but that you
-        # can modify and extend without fucking up the actual class.
+      # Every object has a "singleton" class, which you can think
+      # of as the class (ie, 1.metaclass =~ Fixnum) --- but that you
+      # can modify and extend without fucking up the actual class.
+      def metaclass; class << self; self; end; end
+      def meta_eval(&blk) metaclass.instance_eval &blk; end
+      def meta_def(name, &blk) meta_eval { define_method name, &blk }; end
+      def try(meth, *args); send(meth, *args) if respond_to? meth; end
 
-        def metaclass; class << self; self; end; end
-        def meta_eval(&blk) metaclass.instance_eval &blk; end
-        def meta_def(name, &blk) meta_eval { define_method name, &blk }; end
-        def try(meth, *args); send(meth, *args) if respond_to? meth; end
-
-        def through(meth, *args)
-            if respond_to? meth
-                send(meth, *args)
-            else
-                self
-            end
-        end
+      def through(meth, *args)
+          if respond_to? meth
+              send(meth, *args)
+          else
+              self
+          end
       end
 
       def pbpaste
